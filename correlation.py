7b188dcdb92e159955ada5fe4ba2e5c0f102caa0
wentian49 <wbai1295@gmail.com>
2019-09-23 21:14:20 -0400

    correlation

--- correlation.py
+++ correlation.py
@@ -8,95 +8,41 @@ def bn(a):
         a[i] = (a[i] - np.min(a[i])) / (np.max(a[i]) - np.min(a[i]))
     return a
 
-def correlation(x, y):
-    x_mean = np.mean(x)
-    y_mean = np.mean(y)
 
-    sx = np.std(x)
-    sy = np.std(y)
-
-    s = 0
-    for x1, y1 in zip(x, y):
-        s += (x1 - x_mean) * (y1 - y_mean)
-
-    r = s /  (sx * sy) / (len(x) - 1)
-
-    return r
-
-lstm = np.load('../uav_test/tmpdata/lstmdata.npy')
-cnn = np.load('../uav_test/tmpdata/evaluate_cnn.npy')
-gtr = np.load('../uav_test/tmpdata/y_test.npy')
-# bn(lstm)
-bn(cnn)
-bn(gtr)
-# lstm = lstm[0]
-# cnn = cnn[0]
-# gtr = gtr[0]
-
-# lstm = lstm.reshape((-1))
-# cnn = cnn.reshape((-1))
-# gtr = gtr.reshape((-1))
-
-# print(lstm.shape)
-print(cnn.shape)
-print(gtr.shape)
+prediction = np.load('../../../data/zzhao/uav_regression/prediction.npy')
+label = np.load('../../../data/zzhao/uav_regression/label.npy')
+# v1 model data
+# prediction = np.load('tmpdata/evaluate_cnn.npy')
+# label = np.load('tmpdata/y_test.npy')
+# prediction = prediction[:40]
+# label = label[:40]
 
 
-meanx = []
-meany = []
-for cn, gt in zip(cnn, gtr):
-    cn = cn.reshape((-1))
-    gt = gt.reshape((-1))
-    x_p = []
-    y_p = []
-    for c, g in zip(cn, gt):
-        x_p.append(c)
-        y_p.append(g)
-    meanx.append(x_p)
-    meany.append(y_p)
+bn(prediction)
+bn(label)
 
-meanx = np.mean(meanx, axis=0)
-meany = np.mean(meany, axis=0)
+print(prediction.shape)
+print(label.shape)
 
-meanx = (meanx - np.min(meanx)) / (np.max(meanx) - np.min(meanx))
-meany = (meany - np.min(meany)) / (np.max(meany) - np.min(meany))
 
-r = np.corrcoef(meanx, meany)
+x = prediction.reshape((-1))
+y_true = label.reshape((-1))
+r = np.corrcoef(x, y_true)
 r = r[0,1]
-y = meanx * r
-y = (y - np.min(y)) / (np.max(y) - np.min(y))
-
-# print(stats.mode(meanx)[0][0], stats.mode(meanx)[1])
-# print(stats.mode(meany)[0][0], stats.mode(meany)[0][1], stats.mode(meany)[0][2])
-# np.argmax
-
 print('correlation coefficient : \n', r)
+y_pre = x*r
 
-import collections
-import heapq
- 
-nums = meanx
-k = 5
-count = collections.Counter(nums)  
-out = heapq.nlargest(k, count.keys(), key=count.get)
-print(out)
-
- 
-nums = meany
-k = 5
-count = collections.Counter(nums)  
-out = heapq.nlargest(k, count.keys(), key=count.get)
-print(out)
 
 
 plt.figure()
 palette = plt.get_cmap('Set1')
-plt.plot(meanx, y, color=palette(2),label="correlation", linewidth=1, zorder=-1)
-plt.scatter(meanx, meany, color=palette(0), label="actual", s=1, zorder=1)
+plt.plot(x, y_pre, color='blue',label="correlation", linewidth=1, zorder=1)
+plt.scatter(x, y_true, color=palette(0), label="actual", s=0.1, zorder=-1)
 plt.xlabel("prediction")
-plt.ylabel("groundTrue")
-plt.title('Correlation Coefficient = 0.99')
+plt.ylabel("label")
+plt.title('Correlation Coefficient = {0}'.format(np.round(r,2)))
 plt.legend()
-plt.savefig("img/relation.png")
+plt.savefig("img/relation_no.png")
+# plt.savefig("img/v1_relation.png")
 
 # print(a/b)'''

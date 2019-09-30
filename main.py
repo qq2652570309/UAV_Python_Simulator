f687d064876dc50e87a79f17b4d9aab41d1f2b92
lab49 <wentan@gmail.com>
2019-09-30 10:12:25 -0400

    analysis

--- main.py
+++ main.py
@@ -21,42 +21,53 @@ def averageDensity(gtr, time):
 
 
 if __name__ == "__main__":
-    s = Simulator(time=110)
+    simulateTime = 210
+    s = Simulator(batch=3, time=simulateTime)
     startTimeTotal = time.time()
     s.generate()
 
     print('input shape is {0}\n'.format(s.trajectors.shape))
     
-    stable = s.trajectors[:, 150:200,:]
+    stable = s.trajectors[:, 150:200, :]
     # stableMap = np.zeros(shape=())
-    end = 100
-    interval=10
+    moveStep = 10
+    window = 30
+    end = simulateTime-window
     batchSize = s.trajectors.shape[0]
-    intervalNum = int(end/interval) + 1
+    intervalNum = int(end/moveStep) + 1
 
     analysis = np.zeros(shape=(batchSize, intervalNum, 100, 100))
     print('analysis shape is {0}\n'.format(analysis.shape))
+    
 
-
+    for b in range(len(s.trajectors)):
+        for i in range(0, end+1, moveStep):
+            intervalTrajectory = s.trajectors[b, i:(i+window), :]
+            intervalDensity = generateDensity(intervalTrajectory)
+            intervalDensity = intervalDensity/window
+            intervalDensity = averageDensity(intervalDensity,window)
+            analysis[b, int( (i+(moveStep-1)) / moveStep )] = intervalDensity
+            # print(i, ' : ', int( (i+(moveStep-1)) / moveStep))
     
-    
-    
+    '''
     for b in range(len(s.trajectors)):
         for i in range(0, end+1, interval):
-            intervalTrajectory = s.trajectors[b, i:(i+interval),:]
+            intervalTrajectory = s.trajectors[b, i:(i+interval), :]
             intervalDensity = generateDensity(intervalTrajectory)
             intervalDensity = intervalDensity/interval
             intervalDensity = averageDensity(intervalDensity,interval)
-            analysis[b, int((i+9)/10)] = intervalDensity
-    
+            analysis[b, int( (i+(interval-1)) / interval )] = intervalDensity
+    #'''
     
     for b in range(batchSize):
-        print('samep {0} :'.format(b))
-        for i in range(10):
-            mae = (np.abs(analysis[b, i] - analysis[b, 10])).mean(axis=None) * 100
+        print('sample {0} :'.format(b))
+        for i in range(intervalNum-1):
+            mae1 = (np.abs(analysis[b, i] - analysis[b, intervalNum-1])).mean(axis=None) * 100
+            mae2 = (np.abs(analysis[b, i] - analysis[b, intervalNum-1])).mean(axis=None) * 100
             # mae = np.sum(np.abs(analysis[i] - analysis[10]), axis=None)
-            print('     ', mae)
+            # print('     ', abs(mae1-mae2))
+            print('     ', abs(mae1))
     
-    #'''
+    
 
     

aadbd24aa0fa3f95d9781156d3267869f3452707
lab49 <wentan@gmail.com>
2019-10-06 20:53:50 -0400

    merge

--- simulator3.py
+++ simulator3.py
@@ -35,7 +35,7 @@ class Simulator3:
             trajectors = np.zeros(shape=(self.time, self.map_size, self.map_size), dtype=int)
 
             self.area.refresh(mapSize=self.map_size, areaSize=3, num=10)
-            self.drawPatten_vertical_horizontal(batch_idx)
+            self.drawPatten_horizontal_vertical_horizontal(batch_idx)
             start_time = random.choice(range(0, 80))
             
             # time iteration
@@ -68,7 +68,7 @@ class Simulator3:
                             self.tasks[batch_idx,time_idx,task_idx,2] = endRow
                             self.tasks[batch_idx,time_idx,task_idx,3] = endCol
 
-                        trajectors = self.vertical_horizontal(startRow=startRow, startCol=startCol, 
+                        trajectors = self.horizontal_vertical_horizontal(startRow=startRow, startCol=startCol, 
                                                             endRow=endRow, endCol=endCol, 
                                                             currentTime=currentTime, trajectors=trajectors)
             logging.info('End {0} iteration, cost {1}'.format(batch_idx, time.time() - startTimeIter))
@@ -154,6 +154,76 @@ class Simulator3:
             self.totalFlyingTime += len(c)
         return trajectors
 
+    def horizontal_vertical_horizontal(self, startRow, startCol, endRow, endCol, currentTime, trajectors):
+        midCol = int(round((startCol + endCol) / 2))
+        midRow1 = int(startRow)
+        midRow2 = int(endRow)
+        
+        remainingTime = self.time - currentTime
+        # first horizontal movement
+        if remainingTime >= abs(startCol-midCol)+1 :
+            # enough time for horizontal
+            if startCol < midCol :
+                r1 =  np.arange(startCol, midCol+1, dtype=int)
+            else:
+                r1 = np.arange(midCol, startCol+1, dtype=int)[::-1]
+        else:
+            # not enough time for horizontal
+            if startCol < midCol:
+                r1 = np.arange(startCol, startCol+remainingTime, dtype=int)
+            else:
+                r1 = np.arange(startCol-remainingTime+1, startCol+1, dtype=int)[::-1]
+        t1 = np.arange(currentTime, currentTime+len(r1))
+        trajectors[t1,startRow,r1] += 1
+        remainingTime -= len(r1)
+        self.totalFlyingTime += len(r1)
+        
+        # vertical movement 
+        if remainingTime > 0 :
+            # exists time for vertical
+            if remainingTime >= abs(midRow1-midRow2) :
+                # enough time for vertical
+                if midRow1 < midRow2:
+                    c = np.arange(midRow1+1, midRow2+1, dtype=int)
+                else:
+                    c = np.arange(midRow2, midRow1, dtype=int)[::-1]
+            else:
+                # not enough time for vertical
+                if midRow1 < midRow2:
+                    c = np.arange(midRow1+1, midRow1+remainingTime+1, dtype=int)
+                else:
+                    c = np.arange(midRow1-remainingTime, midRow1, dtype=int)[::-1]
+            t2 = np.arange(t1[-1]+1, t1[-1] + len(c)+1, dtype=int)
+            trajectors[t2, c, midCol] += 1
+            remainingTime -= len(c)
+            self.totalFlyingTime += len(c)
+        
+        # second horizontal movement
+        if remainingTime > 0 :
+            # exists time for horizontal
+            if remainingTime >= abs(midCol-endCol):
+                # enough time for horizontal
+                if midCol < endCol :
+                    r2 =  np.arange(midCol+1, endCol, dtype=int)
+                else:
+                    r2 = np.arange(endCol, midCol, dtype=int)[::-1]
+            else:
+                # not enough time for horizontal
+                if midCol < endCol:
+                    r2 = np.arange(midCol+1, midCol+remainingTime, dtype=int)
+                else:
+                    r2 = np.arange(midCol-remainingTime+1, midCol, dtype=int)[::-1]
+            # check if uav has vertical movement
+            if len(t2) > 0:
+                t3 = np.arange(t2[-1], t2[-1] + len(r2), dtype=int)
+            else:
+                t3 = np.arange(t1[-1]+1, t1[-1] + len(r2)+1, dtype=int)
+            trajectors[t3,endRow,r2] += 1
+            remainingTime -= len(r2)
+            self.totalFlyingTime += len(r2)
+
+        return trajectors
+
     def drawPatten_horizontal_vertical(self, batch_idx):
         startPositions = self.area.getLaunchPoint()
         for startRow, startCol, _ in startPositions:
@@ -187,6 +257,49 @@ class Simulator3:
                 else:
                     r = np.arange(endCol, startCol)[::-1]
                 self.Rfeature[batch_idx, endRow, r] = 1
+
+    def drawPatten_horizontal_vertical_horizontal(self, batch_idx):
+        startPositions = self.area.getLaunchPoint()
+        for startRow, startCol, _ in startPositions:
+            for endRow, endCol in self.area.getDestination(allPoints=True):
+                startRow, startCol = int(startRow), int(startCol)
+                endRow, endCol = int(endRow), int(endCol)
+                if startCol < endCol :
+                    r =  np.arange(startCol, endCol+1)
+                else:
+                    r = np.arange(endCol, startCol+1)[::-1]
+                self.Rfeature[batch_idx, startRow, r, 1] = 1
+                if startRow < endRow:
+                    c = np.arange(startRow+1, endRow+1)
+                else:
+                    c = np.arange(endRow, startRow)[::-1]
+                self.Rfeature[batch_idx, c, endCol, 1] = 1
+                
+                
+                midCol = round((startCol + endCol) / 2)
+                midRow1 = startRow
+                midRow2 = endRow
+                # first horizontal movement
+                if startCol < midCol :
+                    r1 =  np.arange(startCol, midCol+1, dtype=int)
+                else:
+                    r1 = np.arange(midCol, startCol+1, dtype=int)[::-1]
+                self.Rfeature[batch_idx, startRow, r1, 1] = 1
+                
+                # vertical movement 
+                # enough time for vertical
+                if midRow1 < midRow2:
+                    c = np.arange(midRow1+1, midRow2+1, dtype=int)
+                else:
+                    c = np.arange(midRow2, midRow1, dtype=int)[::-1]
+                self.Rfeature[batch_idx, c, midCol, 1] = 1
+                
+                # second horizontal movement
+                if midCol < endCol :
+                    r2 =  np.arange(midCol+1, endCol, dtype=int)
+                else:
+                    r2 = np.arange(endCol, midCol, dtype=int)[::-1]
+                self.Rfeature[batch_idx, endRow, r2, 1] = 1
 
 
 if __name__ == "__main__":
